080b78a18a2eb510b1cfb0adb78967d8888120a2
vladimircabrera <vladimir.cv.21@gmail.com>
2017-04-27 13:43:16 -0300

    Update ejercicio1.c

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,1 +1,21 @@
-# include
+# include <stdio.h>
+
+# define MSJ_ING_VAR_BASE     "Ingrese el valor de la base del rectangulo"
+# define MSJ_ING_VAR_ALTURA   "Ingrese el valor de la altura del rectangulo"
+# define MSJ_PERIMETRO        "El perimetro del rectangulo es:"
+
+int main (void)
+{
+  float ladoA, ladoB;
+  
+  puts (MSJ_ING_VAR_BASE);
+  scanf ("%f", &ladoA);
+  puts (MSJ_ING_VAR_ALTURA);
+  scanf ("%f", &ladoB);
+  
+  printf ("%s %f\n", MSJ_PERITRO, ladoA + ladoB);
+  
+  return 0;
+  
+}
+

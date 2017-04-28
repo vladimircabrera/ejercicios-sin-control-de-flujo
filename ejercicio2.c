9404597ffdec8a817da2ce7d8ce2395f1402a0cf
vladimircabrera <vladimir.cv.21@gmail.com>
2017-04-27 22:40:46 -0300

    Update ejercicio2.c

--- ejercicio2.c
+++ ejercicio2.c
@@ -1,6 +1,19 @@
 # include <stdio.h>
 
+# define MSJ_INGRE_HORA "Ingrese la hora actual"
+# define MSJ_INGRE_MIN  "Ingrese los minutos actuales"
+# define MSJ_INGRE_SEG  "Ingrese los segundos actuales"
+
 int main (void)
 {
+  short hh, mm, ss;
   
+  puts (MSJ_INGRE_HORA);
+  scanf ("%d", &hh);
+  puts (MSJ_INGRE_MIN);
+  scanf ("%d", &mm);
+  puts (MSJ_INGRE_SEG);
+  scanf ("%d", &ss);
+  
+  printf ()
 }
